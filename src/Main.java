5b67832be0c44986512a67d2ac383abc24b27e70
Samadhan Bhusnar <samadhanbhusnar21@gmail.com>
2026-06-02 13:01:26 +0530

    stack program completed in java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,17 @@
+import stack.*;
+
 public class Main   {
     public static void main(String[] args) {
         // array data structure
-//         ArrayDS.eventDrivenProgram(); // array
-        // stack data structure
-//        StackLocal.eventDrivenProgram(); // stack
-        // infix to postfix
-//        InfixToPostfix.infixToPostfix(); // infix to postfix
-        // infix to prefix
-//        InfixToPrefix.infixToPrefix(); // infix to prefix
-        // matching parenthesis
-//        MatchingParenthesis.checkExpressionIsValid(); // matching parenthesis
+         ArrayDS.eventDrivenProgram(); // array
+//         stack data structure
+        StackLocal.eventDrivenProgram(); // stack
+//         infix to postfix
+        InfixToPostfix.infixToPostfix(); // infix to postfix
+//         infix to prefix
+        InfixToPrefix.infixToPrefix(); // infix to prefix
+//         matching parenthesis
+        MatchingParenthesis.checkExpressionIsValid(); // matching parenthesis
         // evaluate postfix
         Evaluation.evaluatePostfixExpression(); // evaluate postfix
     }
